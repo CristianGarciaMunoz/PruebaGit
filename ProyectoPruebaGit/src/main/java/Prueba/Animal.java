742601491d3ff2cdadeb8b0c3767d5ab025a50c9
ericjose2007 <ericjosemunoz7@gmail.com>
2026-06-02 09:55:44 +0200

    Constructores
    Creo los 2 tipos de constructores

--- ProyectoPruebaGit/src/main/java/Prueba/Animal.java
+++ ProyectoPruebaGit/src/main/java/Prueba/Animal.java
@@ -8,5 +8,17 @@ public class Animal {
     protected String dieta;
     protected String nombre;
     protected int edad;  
+
+    public Animal animal() {
+        this.dieta = "";
+        this.nombre = "";
+        this.edad = 0;
+    }
+
+    public Animal animal(String dieta, String nombre, int edad) {
+        this.dieta = dieta;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
     
 }
