e8b4595dc718517de841bdc778ad4ef3c501a024
CristianGarciaMunoz <cristiangarciamunoz1@gmail.com>
2026-06-02 10:07:00 +0200

    Commit3

--- ProyectoPruebaGit/src/main/java/Prueba/Animal.java
+++ ProyectoPruebaGit/src/main/java/Prueba/Animal.java
@@ -9,16 +9,29 @@ public class Animal {
     protected String nombre;
     protected int edad;  
 
-    public Animal animal() {
+    public Animal() {
         this.dieta = "";
         this.nombre = "";
         this.edad = 0;
     }
 
-    public Animal animal(String dieta, String nombre, int edad) {
+    public Animal(String dieta, String nombre, int edad) {
         this.dieta = dieta;
         this.nombre = nombre;
         this.edad = edad;
     }
-    
+
+    public void setDieta(String dieta) {
+        this.dieta = dieta;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void respirar() {}
 }
